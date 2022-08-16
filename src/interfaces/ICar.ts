f27112d3a82f629e0686d2bf97f0656b3b4f76e2
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-08-16 16:23:41 -0300

    refactor: numero de assentos de  ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -3,7 +3,7 @@ import { vehicleZodSchema } from './IVehicle';
 
 const carZodSchema = vehicleZodSchema.extend({
   doorsQty: z.number().gte(2).lte(4),
-  seatsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
 });
 
 export type ICar = z.infer<typeof carZodSchema>;
